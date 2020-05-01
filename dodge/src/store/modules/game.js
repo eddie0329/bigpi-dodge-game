f5d414d68b796eb2486be7353627443235aca9b0
eddie <fdsa0329@naver.com>
2020-05-01 19:59:12 +0900

    :construction: WORKING

--- dodge/src/store/modules/game.js
+++ dodge/src/store/modules/game.js
@@ -1,21 +1,38 @@
+const MUTATION_CONSTANTS = {
+  INIT_GAME: "INIT_GAME",
+  FINISH_GAME: "FINISH_GAME",
+};
+
 const state = {
   gameStatus: {
-    collision: false
-  }
+    gameStart: false,
+    collision: false,
+    score: 0,
+  },
 };
 
 const getters = {
-  getGameStatus: state => state.gameStatus
+  getGameStatus: (state) => state.gameStatus,
 };
 
-const mutations = {};
+const mutations = {
+  [MUTATION_CONSTANTS.INIT_GAME]() {},
+  [MUTATION_CONSTANTS.FINISH_GAME]() {},
+};
 
-const actions = {};
+const actions = {
+  startGame({ commit }) {
+    commit(MUTATION_CONSTANTS.INIT_GAME);
+  },
+  endGame({ commit }) {
+    commit(MUTATION_CONSTANTS.FINISH_GAME);
+  },
+};
 
 export default {
   namespaced: true,
   state,
   getters,
   mutations,
-  actions
+  actions,
 };

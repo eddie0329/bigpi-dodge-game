fe3f115c7036e132dc349d6e203bce83b20b5cb2
eddie <fdsa0329@naver.com>
2020-05-24 00:17:04 +0900

    :whale: Add dockerfiles and compose

--- back/App.js
+++ back/App.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
+const dbUrl = `mongodb://${dbUser}:${dbPass}@${dbHost}/${dbName}`;
+console.log("try connecting to", dbUrl);
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+console.log("connected!");
 
 app.get("/", (req, res) => {
   res.send("HELLO");
